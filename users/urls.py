83048b914be625180cfeefd49144395a40c5dce0
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-29 01:50:37 +0300

    user register, login, logout, reset password done

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,22 @@
+
 from django.urls import path
 
-from users.views import user_login
+from users.views import register, user_login, user_logout
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-path("login/", user_login, "user_login.html")
-
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("register/", register, name="register"),
+    path('change-password/', auth_views.PasswordChangeView.as_view(
+        template_name='users/change-password.html'), name="change-password"),
+    path('reset-password/', auth_views.PasswordResetView.as_view(
+        template_name='users/reset-password.html'), name="reset-password"),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/reset_password_sent.html'), name="password_reset_done"),
+    path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/change-password.html'), name="password_reset_confirm"),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/reset_password_sucess.html'), name="password_reset_complete"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import  AuthenticationForm
+from django.contrib.auth import  login, logout
+from django.contrib import messages
 
+from users.forms import UserForm
 # Create your views here.
 
 
 def user_logout(request):
-    pass
+    messages.success(request, 'You logged out!')
+    logout(request)
+    # return redirect('logout')
+    return render(request, 'users/user_logout.html')
+
 
 def register(request):
-    pass
+    form_user = UserForm()
+    if request.method == 'POST':
+        form_user = UserForm(request.POST)
+        if form_user.is_valid():
+            user = form_user.save()
+            login(request, user)
+            messages.success(request, 'Register Successfull!')
+            return redirect('home')
+    context = {
+        "form_user": form_user,
+    }
+
+    return render(request, 'users/register.html', context)
+
 
 def user_login(request):
-    return render(request, 'login.html')
+    form = AuthenticationForm(request, data=request.POST)
+
+    if form.is_valid():
+        user = form.get_user()
+        
+        if user:
+            messages.success(request, "Login Successfull")
+            login(request,user)
+            return redirect('home')
+
+    return render(request, 'users/user_login.html', {"form":form})
