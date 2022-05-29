8ec07843fb99652d1b09d338bff13a13d75417b5
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-29 23:18:37 +0300

    simple crud operations added

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+from blog.views import PostAddView, PostDetailView, PostListView
 
+urlpatterns = [
+    path("<slug:slug>", PostDetailView.as_view(), name="post_detail"),
+    path("", PostListView.as_view(), name="post_list"),
+    path('add/', PostAddView.as_view(), name="add"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from blog.forms import PostForm
 
+from blog.models import Post
 # Create your views here.
 
 def home(request):
     return render(request, 'blog/home.html')
+
+class PostListView(ListView):
+    model = Post
+    template_name = "post_list.html"
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+
+class PostAddView(CreateView):
+    model= Post
+    form_class= PostForm
+    template_name= 'blog/post_create.html' 
+    success_url = reverse_lazy("home")
