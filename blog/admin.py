2dccc762739699e79f01c69b7827a05f034dc857
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-29 23:16:50 +0300

    auto slug to post for admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from blog.models import Post
+
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "content",)
+    prepopulated_fields = {"slug": ("title",)}  # new
+
+admin.site.register(Post, PostAdmin)
