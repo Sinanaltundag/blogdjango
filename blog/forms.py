3ea75cc0923a091656529602cc0ebafcef69224a
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-30 15:30:03 +0300

    blog post create fix some bugs

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = '__all__' 
+        exclude = ('slug', 'user')
--- blog/models.py
+++ blog/models.py
@@ -7,13 +7,21 @@ from django.template.defaultfilters import slugify
 class Category(models.Model):
     name= models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content=models.TextField()
     image=models.ImageField(upload_to="blog_pics", blank=True)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    status = models.BooleanField(default=True)
+    STATUS_CHOICES=(
+        ("1",'published'), 
+        ("2",'draft'), 
+        ("3",'pending')
+        )
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
     slug = models.SlugField(null=False, unique=True)
     user = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
@@ -23,7 +31,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("post_detail", args={"slug": self.slug})
+        return reverse("post_detail", kwargs={"slug": self.slug})
 
     def save(self, *args, **kwargs):  # new
         if not self.slug:
@@ -36,6 +44,9 @@ class Comment(models.Model):
     content = models.TextField()
     user= models.ForeignKey(User, on_delete=models.CASCADE)
     post= models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:50]
 
 class Like(models.Model):
     post= models.ForeignKey(Post, on_delete=models.CASCADE)
