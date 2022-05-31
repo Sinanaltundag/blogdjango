0c98c80408e5fbd0756831d5758b789c7d791d74
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-31 22:03:38 +0300

    update url added, detail view changed

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from blog.views import PostAddView, PostDeleteView, PostDetailView, PostListView
+from blog.views import PostAddView, PostDeleteView, PostDetailView, PostListView, PostUpdateView, detailView
 
 urlpatterns = [
-    path("<slug:slug>", PostDetailView.as_view(), name="post_detail"),
-    path("", PostListView.as_view(), name="post_list"),
+    # path("<slug:slug>", PostDetailView.as_view(), name="post_detail"),
+    path("<slug:slug>", detailView, name="post_detail"),
+    # path("", PostListView.as_view(), name="post_list"),
     path('add/', PostAddView.as_view(), name="post_add"),
+    path('post-update/<int:pk>', PostUpdateView.as_view(), name="post_update"),
     path('post-delete/<int:pk>', PostDeleteView.as_view(), name="post_delete"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
