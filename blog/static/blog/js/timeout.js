c6e2276cccdaecc85c578bad1a82c903590360ff
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-03 11:45:10 +0300

    change static path

--- blog/static/blog/js/timeout.js
+++
@@ -1,5 +0,0 @@
-let element = document.querySelector('.alert');
-
-setTimeout(function () {
-  element.style.display = 'none';
-}, 3000);
--- main/settings.py
+++ main/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -139,3 +139,8 @@ MEDIA_URL = '/media/'
 # LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
